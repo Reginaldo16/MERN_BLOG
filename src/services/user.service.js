0f5403de7f586d39f082e241e4810f916f6527c7
Reginaldo16 <Reginaldotaylor16@gmail.com>
2025-07-20 16:54:35 -0500

    service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -5,11 +5,15 @@ import User from "../models/User.js";
 const findAllUsers = () => User.find();
 const findById = (id) => User.findById(id)
 const create = (body) => User.create(body)
+const update = (id, name, username, email, avatar, background) => User.findOneAndUpdate({ _id: id, }, {
+    name, username, email, avatar, background
+})
 
 
 
 export default {
     create,
     findAllUsers,
-    findById
+    findById,
+    update
 }
