8b2b09a1b32aa0bc02d18970b7f665207f707a08
Reginaldo16 <Reginaldotaylor16@gmail.com>
2025-07-19 22:17:36 -0500

    feat: findbyid

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,17 @@ const getAllUsers = (req, res) => {
     res.status(200).json({ message: 'users' });
 };
 
+const findById = (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ error: 'ID inválido' });
+    }
+
+    res.status(200).json({ message: `Usuario com id  ${id}` });
+};
+
 export default {
-    getAllUsers
+    getAllUsers,
+    findById
 };
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,5 +4,6 @@ import userController from '../controllers/user.controller.js'
 
 const route = Router();
 route.get('/', userController.getAllUsers)
+route.get('/:id', userController.findById)
 
 export default route
