f09404af089476e88fcad6887280c22d84562fc4
Reginaldo16 <Reginaldotaylor16@gmail.com>
2025-07-20 15:42:45 -0500

    findbyid service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,15 @@
 import User from "../models/User.js";
 
 
+
+const findAllUsers = () => User.find();
+const findById = (id) => User.findById(id)
 const create = (body) => User.create(body)
 
 
 
 export default {
-    create
+    create,
+    findAllUsers,
+    findById
 }
